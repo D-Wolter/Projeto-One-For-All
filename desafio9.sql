07bbc0e87cb13ac5a14d38f4bbae746cd31072f8
D-Wolter <wolter.daniel@gmail.com>
2022-12-06 11:41:27 -0300

    req9

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT
   COUNT(u.user_name) quantidade_musicas_no_historico
-FROM reproductions_history rh
+FROM history rh
 INNER JOIN users u ON u.user_id = rh.user_id
 WHERE u.user_name = "Barbara Liskov";
