4e5ce48534a3bf0b1329f6a3edc4fc0187f9c439
D-Wolter <wolter.daniel@gmail.com>
2023-03-06 15:01:42 -0300

    feat: REQ10 - Normalize a tabela da planilha SpotifyClone-fav-songs para 3 forma normal

--- challenges/desafio10.sql
+++ challenges/desafio10.sql
@@ -1,13 +1,24 @@
--- Descomente e altere as linhas abaixo:
 
--- CREATE TABLE SpotifyClone.tabela(
---     coluna1 tipo restricoes,
---     coluna2 tipo restricoes,
---     colunaN tipo restricoes,
--- ) engine = InnoDB;
+CREATE TABLE IF NOT EXISTS cancoes_favoritas(
+    musica_id INT NOT NULL,
+    usuario_id INT NOT NULL,
+    CONSTRAINT PRIMARY KEY(usuario_id, musica_id),
+    FOREIGN KEY(usuario_id) REFERENCES usuario(usuario_id),
+    FOREIGN KEY(musica_id) REFERENCES musica(musica_id)
+);
 
--- INSERT INTO SpotifyClone.tabela (coluna1, coluna2)
--- VALUES
---   ('exemplo de dados 1', 'exemplo de dados A'),
---   ('exemplo de dados 2', 'exemplo de dados B'),
---   ('exemplo de dados 3', 'exemplo de dados C');
+INSERT INTO cancoes_favoritas (usuario_id, musica_id)
+VALUES
+  (1, 3),
+  (1, 6),
+  (1, 10),
+  (2, 4),
+  (3, 1),
+  (3, 3),
+  (4, 7),
+  (4, 4),
+  (5, 10),
+  (5, 2),
+  (8, 4),
+  (9, 7),
+  (10, 3);
